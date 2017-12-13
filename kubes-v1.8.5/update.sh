c0115f234531c626ebf432446391919b5362f218
liuyong@chinacloud.com.cn <liuyong@chinacloud.com.cn>
2017-12-13 16:39:01 +0800

    update

--- kubes-v1.8.5/update.sh
+++ kubes-v1.8.5/update.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+all_file=./Dockerfile
+ > $all_file
 for line in `cat ./v1.8.5.info`
 do
   name=`echo $line | awk -F '/' '{print $3}' | awk -F ':' '{print $1}'`
   echo $name
   echo $line
   echo "FROM "$line > $name
+  echo "FROM "$line >> $all_file
 done
